2c235db1ec0268f878896148bdcaa0c4bdbf138f
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-05-22 20:35:12 +0200

    change function and fix nodemailer ports

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = env.parsed.PORT
 const nodeMailerRoutes = require("./routes/v1/SendMail");
-app.use("/api/v1/", nodeMailerRoutes);
+app.use(`/api/v1/`, nodeMailerRoutes);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
