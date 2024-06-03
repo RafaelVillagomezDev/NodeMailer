9333fda81e3f5d2f65b21321205d9e7f53c340a1
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-06-03 20:18:45 +0200

    Arreglo en la funcion encryptar

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,80 @@
+const CryptoJS = require('crypto-js');
+const env=require("dotenv").configDotenv();
+
+const secretKeyEnv= env.parsed.PASSWORD_REQUEST
+
+// Middleware para cifrar los encabezados y el cuerpo
+const authMiddleware = (req, res, next) => {
+    const { headers, body } = req;
+    
+    // Verificar la clave secreta
+    const secretKey = secretKeyEnv; // Obtener la clave secreta del entorno
+    const secretKeyHeader=req.headers["secret-key"]
+
+    if (secretKey!=secretKeyHeader) {
+      return res.status(401).json({ error: 'No tienes autorizacion Api' });
+    }
+
+    // Generar una clave aleatoria para el cifrado
+    const randomWord = CryptoJS.lib.WordArray.random(16);
+
+    // Cifrar los encabezados
+    const encabezadoOriginal = JSON.stringify(headers);
+    const encabezadoCifrado = CryptoJS.AES.encrypt(encabezadoOriginal, secretKey, { iv: randomWord }).toString();
+  
+    // Establecer el encabezado cifrado en la solicitud
+    req.headers['secret-key'] = encabezadoCifrado;
+  
+    // Cifrar el cuerpo
+    const cuerpoOriginal = JSON.stringify(body);
+    const cuerpoCifrado = CryptoJS.AES.encrypt(cuerpoOriginal, secretKey, { iv: randomWord }).toString();
+  
+    // Establecer el cuerpo cifrado en la solicitud
+    req.body = cuerpoCifrado;
+  
+    next();
+};
+
+
+// Middleware para descifrar los encabezados y el cuerpo de la solicitud
+const decryptAuthMiddleware = (req, res, next) => {
+    const encabezadoCifrado = req.headers['secret-key'];
+    const cuerpoCifrado = req.body;
+  
+    // Verificar si los encabezados cifrados o el cuerpo cifrado están presentes
+    if (!encabezadoCifrado || !cuerpoCifrado) {
+      return res.status(400).json({ error: 'Encabezados o cuerpo cifrados no encontrados en la solicitud' });
+    }
+  
+    // Verificar la clave secreta
+    const secretKey = secretKeyEnv; // Obtener la clave secreta del entorno
+
+
+
+    try {
+      // Descifrar los encabezados
+      const bytesEncabezado = CryptoJS.AES.decrypt(encabezadoCifrado, secretKey);
+      const encabezadoDescifrado = JSON.parse(bytesEncabezado.toString(CryptoJS.enc.Utf8));
+      if (secretKey!=encabezadoDescifrado) {
+        return res.status(401).json({ error: 'No tienes autorizacion' });
+      }
+      // Establecer los encabezados descifrados en la solicitud
+      req.headers = encabezadoDescifrado;
+  
+      // Descifrar el cuerpo
+      const bytesCuerpo = CryptoJS.AES.decrypt(cuerpoCifrado, secretKey);
+      const cuerpoDescifrado = JSON.parse(bytesCuerpo.toString(CryptoJS.enc.Utf8));
+  
+      // Establecer el cuerpo descifrado en la solicitud
+      req.body = cuerpoDescifrado;
+  
+      next();
+    } catch (error) {
+      // Manejar el error de descifrado
+      return res.status(400).json({ error: 'Error al descifrar los datos cifrados' });
+    }
+};
+
+  
+ module.exports ={authMiddleware,decryptAuthMiddleware};
+  
--- routes/v1/SendMail.js
+++ routes/v1/SendMail.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const mailerController=require("../../controller/mailerController")
 const validateEmail=require("../../validators/email")
-
-router.post("/send",validateEmail("send"),mailerController.sendMail)
+const authMiddleware=require("../../middleware/authMiddleware")
+router.post("/send",authMiddleware.authMiddleware,authMiddleware.decryptAuthMiddleware,validateEmail("send"),mailerController.sendMail)
 
 
 module.exports=router
