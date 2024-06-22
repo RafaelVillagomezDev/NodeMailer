07626adeb2d6d8b26939254dc3217efd5df29839
Rafael Villagomez <102181526+RafaelVillagomezDev@users.noreply.github.com>
2024-06-22 22:38:57 +0200

    añadir cors import

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const env=require("dotenv").configDotenv()
 const bodyParser = require("body-parser");
 const app = express();
-
+const cors = require('cors');
 
 const whitelist = ['http://yandrydev.es', 'https://yandrydev.es'];
 
